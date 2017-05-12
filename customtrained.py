189a18665c5923f011fcbbe1a0b6be64810ffcc7
Rajat Bhati <rajat.bhati1995@gmail.com>
2017-05-12 10:21:16 -0700

    model train

---
+++ customtrained.py
@@ -0,0 +1,95 @@
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+import cv2, numpy as np
+import pandas
+
+def VGG_19(weights_path=None):
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1000, activation='softmax'))
+
+    
+    return model
+
+
+model = VGG_19()
+dataframe = pandas.read_csv("trainLabels.csv", header=None)
+dataset = dataframe.values
+Y = dataset[:,1:]
+ 
+
+# in deployment we created a folder one of various datasets like cifar stanford car dataset and also batch images from google image search 
+# here will load them and run them based on a trainLabels file 
+# from the labels we checked using reg exp which entries are vehicles and which are not
+
+# we used box implementation of this entry to calculate the portion of car in screen
+# based on that portion we estimated the distance roughly  
+# the project is still under developmnt hence none of the code is final
+from scipy.misc import imread            
+from os import listdir
+import os
+import re
+X=[]                           
+Y_t=[]
+for i in listdir(os.getcwd()+"/train/"):
+    try:
+        a=scipy.misc.imread(os.getcwd()+"/train/"+i)
+    except:
+        continue
+    else:
+        Y_t.append(1 if re.search('car|vehi|train|bus|bike',Y[int(i[:-4])]) else 0)
+        X.append(a)
+X=np.array(X)
+Y=np.array(Y_t)
+
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(optimizer=sgd, loss='categorical_crossentropy')
+out = model.predict(im)
