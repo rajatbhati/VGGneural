0125b9d8077ecb630712a54aa37971570c8f3771
Rajat Bhati <rajat.bhati1995@gmail.com>
2017-07-01 09:08:18 -0700

    pre trained usage

--- train.py
+++ train.py
@@ -4,37 +4,20 @@ from convnetskeras.convnets import preprocess_image_batch, convnet
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 model = convnet('vgg_19',weights_path="weights/vgg19_weights.h5", heatmap=False)
 model.compile(optimizer=sgd, loss='mse')
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-
-
-
+import PIL
 from PIL import Image
-import numpy as np
-import scipy
-import os
-X=[]                           
-Y=[]
-c=-1
-for i in x_train:
-	c+=1
-    try:
-        img=Image.fromarray(img.astype('uint8'),'RGB')
-        new_width=224
- 		new_height=224
-		depth=3
-		img = Image.open('somepic.jpg')
-		img = img.resize((new_width, new_height), Image.ANTIALIAS)
-		img=np.reshape(np.array(list(img.getdata())),(new_width,new_height,depth))
-	    X.append(img)
-        Y.append(y_train[0])
-
-      
-
-    except:
-        continue
-    else:
-X=np.array(X)
-Y=np.array(Y)
-
-
-
+new_width=200
+new_height=200
+depth=3
+img = Image.open(image)
+img = img.resize((new_width, new_height), Image.ANTIALIAS)
+img=np.reshape(np.array(list(img.getdata())),(new_width,new_height,depth))
+preds = model.predict(image)
+P = imagenet_utils.decode_predictions(preds)
+v=0
+import re
+for i in P:
+	if re.search('car|vehi|train|bus|bike',i[0],re.M|re.I) and p[1] > .3:
+		v=1
+return v
+ 
